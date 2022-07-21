64e52f06a17dd32ef28b1e6738ee620b686c5320
JohnKamaujk <johnjkkamau@gmail.com>
2022-07-21 17:31:38 +0300

    Done.

---
+++ src/context/user.js
@@ -0,0 +1,17 @@
+import React,{useState} from "react";
+import data from "../data";
+
+const UserContext = React.createContext();
+
+function UserProvider({ children }) {
+
+    const [user, setUser] = useState(data);
+  
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export { UserContext, UserProvider };
